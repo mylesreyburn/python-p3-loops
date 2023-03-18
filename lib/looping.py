fd19ca8394083889480ae52140d4efb453dc4370
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-18 17:55:21 -0600

    done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_ints = [int * int for int in int_list]
+    return squared_ints
 
 def fizzbuzz():
-    # code goes here!
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
     pass
